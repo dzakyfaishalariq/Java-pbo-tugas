6e90c2f334652b7c3ff45207b7907ca276ff449d
DzakyFaishalariq <84628553+dzakyfaishalariq@users.noreply.github.com>
2022-04-10 08:13:45 +0700

    tugas bu arie PBO

--- practic_7/practic7/src/com/prt7/arcad_libray/arcad.java
+++ practic_7/practic7/src/com/prt7/arcad_libray/arcad.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class arcad {
     private int totalNilaiKartu;
+    private int totalNilaiKartu2;
     private String kartuUser;
+    private String kartuUser2;
     private int totalUangUser;
     private int[] hargaKart = new int[5];
 
-    public arcad(String kartuUser, int totalUangUser) {
+    public arcad(String kartuUser, String kartuUser2, int totalUangUser) {
         this.kartuUser = kartuUser;
+        this.kartuUser2 = kartuUser2;
         this.totalNilaiKartu = 0;
+        this.totalNilaiKartu2 = 0;
         this.totalUangUser = totalUangUser;
         this.hargaKart[0] = 100;
         this.hargaKart[1] = 200;
@@ -20,6 +24,7 @@ public class arcad {
     }
 
     public void pilihNilaiKartu() {
+        System.out.println("================================================================");
         Scanner input = new Scanner(System.in);
         System.out.println("Pilih menu nilai kartu permainan yang di butuhkan!");
         System.out.println("1. poin = 100 harga = Rp. 10.000");
@@ -50,6 +55,7 @@ public class arcad {
     }
 
     public void tampilkanKembalian() {
+        System.out.println("================================================================");
         if (this.totalUangUser <= 0) {
             System.out.println("Saldo anda bernilai 0");
             this.totalUangUser = 0;
@@ -60,7 +66,8 @@ public class arcad {
         }
     }
 
-    public void memainkan_permainan(int pilih) {
+    public void memainkan_permainan() {
+        System.out.println("================================================================");
         char p = 'y';
         while (p == 'y') {
             Scanner input = new Scanner(System.in);
@@ -70,19 +77,89 @@ public class arcad {
             System.out.println("[2]. Permainan pesawat 10 koin");
             System.out.println("[3]. Permainan kartu 5 koin");
             System.out.println("[4]. Permainan menari 5 koin");
-            System.out.print("Masukkan pilihan anda: ");
-            int pilihan = input.nextInt();
-            switch (pilihan) {
-                case 1:
-
-                    break;
-                case 2:
-
-                    break;
-                default:
-                    System.out.println("Pilihan tidak ada!");
-                    break;
+            if (this.totalNilaiKartu > 0) {
+                System.out.print("Masukkan pilihan anda: ");
+                int pilihan = input.nextInt();
+                switch (pilihan) {
+                    case 1:
+                        System.out.println("Anda memilih permainan bola 20 koin");
+                        System.out.println("Silahkan masukkan jumlah koin yang ingin anda gunakan ! ");
+                        System.out.print("status transaksi -> ");
+                        this.totalNilaiKartu -= 20;
+                        System.out.println("transaksi berhasil");
+                        break;
+                    case 2:
+                        System.out.println("Anda memilih permainan pesawat 10 koin");
+                        System.out.println("Silahkan masukkan jumlah koin yang ingin anda gunakan ! ");
+                        System.out.print("status transaksi -> ");
+                        this.totalNilaiKartu -= 10;
+                        System.out.println("transaksi berhasil");
+                        break;
+                    case 3:
+                        System.out.println("Anda memilih permainan kartu 5 koin");
+                        System.out.println("Silahkan masukkan jumlah koin yang ingin anda gunakan ! ");
+                        System.out.print("status transaksi -> ");
+                        this.totalNilaiKartu -= 5;
+                        System.out.println("transaksi berhasil");
+                        break;
+                    case 4:
+                        System.out.println("Anda memilih permainan menari 5 koin");
+                        System.out.println("Silahkan masukkan jumlah koin yang ingin anda gunakan ! ");
+                        System.out.print("status transaksi -> ");
+                        this.totalNilaiKartu -= 5;
+                        System.out.println("transaksi berhasil");
+                        break;
+                    default:
+                        System.out.println("Pilihan tidak ada!");
+                        break;
+                }
+                System.out.println("total koin anda adalah : " + this.totalNilaiKartu);
+            } else {
+                System.out.println("Anda tidak memiliki koin yang cukup untuk memainkan permainan");
+                System.out.println("Silahkan isi ulang koin anda");
             }
+            System.out.print("Apakah anda ingin memainkan permainan lagi ? (y/n) : ");
+            p = input.next().charAt(0);
         }
+    }
+
+    public void transfer_saldo() {
+        System.out.println("================================================================");
+        Scanner a = new Scanner(System.in);
+        System.out.println("Silahkan masukkan jumlah saldo yang ingin anda transfer di koin kartu anda");
+        System.out.print("Masukkan jumlah saldo yang ingin anda transfer : ");
+        int saldo = a.nextInt();
+        this.totalNilaiKartu -= saldo;
+        if (this.totalNilaiKartu > 0) {
+            this.totalNilaiKartu2 += saldo;
+            System.out.println("Transaksi berhasil");
+            System.out.println("Saldo anda bernilai               : " + this.totalNilaiKartu);
+            System.out.println("Saldo anda di kartu lain bernilai : " + this.totalNilaiKartu2);
+        } else {
+            System.out.println("Anda tidak memiliki koin yang cukup untuk melakukan transfer");
+        }
+    }
+
+    public void mintak_hadia1() {
+        System.out.println("================================================================");
+        System.out.println("Anda memiliki hadiah 1 dengan penambahan saldo koin 200");
+        this.totalNilaiKartu2 += this.hargaKart[1];
+        System.out.println("Saldo kartu 2  anda bernilai :  " + this.totalNilaiKartu2);
+    }
+
+    public void mintak_hadia2() {
+        System.out.println("================================================================");
+        System.out.println("Anda memiliki hadiah 2 dengan penambahan saldo koin 100");
+        this.totalNilaiKartu += this.hargaKart[0];
+        System.out.println("Saldo kartu 1  anda bernilai :  " + this.totalNilaiKartu);
+    }
+
+    public void tampilkan_semua_nilaiKartu_anda() {
+        System.out.println("================================================================");
+        System.out.println("Nama kartu pertama anda     : " + this.kartuUser);
+        System.out.println("Nama kartu kedua anda       : " + this.kartuUser2);
+        System.out.println("Saldo kartu 1 anda bernilai : " + this.totalNilaiKartu);
+        System.out.println("Saldo kartu 2 anda bernilai : " + this.totalNilaiKartu2);
+        System.out.println("Saldo uang anda bernilai    : " + this.totalUangUser);
     }
 }
