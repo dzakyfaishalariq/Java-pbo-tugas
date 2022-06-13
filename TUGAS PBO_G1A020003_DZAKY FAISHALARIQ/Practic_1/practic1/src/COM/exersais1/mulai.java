04860c5eab88bea3c608d7f91fea1bdc8a32b230
Dzaky_Faishalariq003 <dzakyfaishalariq@gmail.com>
2022-06-13 07:03:01 +0700

    tugas PBO

---
+++ TUGAS PBO_G1A020003_DZAKY FAISHALARIQ/Practic_1/practic1/src/COM/exersais1/mulai.java
@@ -0,0 +1,12 @@
+package COM.exersais1;
+/*
+ * Nama practic : Exersais 1
+ * Nama         : Dzaky Faishalariq
+ * NPM          : G1A020003
+ * Kelas        : Informatika A
+ */
+public class mulai {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
